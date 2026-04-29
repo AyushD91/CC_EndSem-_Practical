d84d34b2b477edf7dfef363403cc4c57b9c47ded
IGNITE07 <akd.dwivedi.sk@gmail.com>
2026-04-29 15:11:59 +0530

    Add files via upload

---
+++ subset_sum.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (arr[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][sum];
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> arr = {3, 34, 4, 12, 5, 2};
+    int sum = 9;
+
+    if (obj.isSubsetSum(arr, sum))
+        cout << "Subset exists\n";
+    else
+        cout << "Subset does not exist\n";
+
+    return 0;
+}
